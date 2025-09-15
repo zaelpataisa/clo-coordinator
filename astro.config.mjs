e3e7c95063e17d81f345e06b6aa8761540fad611
zaelpataisa <zaelaisa_micronoft@hotmail.com>
2025-09-15 09:57:33 -0400

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  })
+});
---
+++ src/utils/routes.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  INDEX: '/',
+  LOGIN: (slug: string) => '/login/${user}',
+  HOME: '/home',
+  PEDIDOS: '/pedidos',
+  FACTURACION: '/facturacion',
+  COBRANZA: '/cobranza',
+  RECLAMOS: '/reclamos',
+  CLIENTES: '/clientes',
+  VENDEDORES: '/vendedores',
+}
